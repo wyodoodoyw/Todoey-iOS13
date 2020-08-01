64a173ce39139d75cb14dffd35c6c34bdfee3089
wyodoodoyw <mwood051@gmail.com>
2020-07-31 20:05:53 -0400

    implement TableViewDataSource methods
    numberOfRows
    cellForRowAt

---
+++ Todoey/Controller/ToDoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Feed 咪猴桃", "Brush 咪猴桃", "Cuddle with 咪猴桃"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    // MARK: - TableView DataSource Methods
+    
+    // Return the number of rows for the table.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    // Provide a cell object for each row.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       // Fetch a cell of the appropriate type.
+       let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+       
+       // Configure the cell’s contents.
+        cell.textLabel!.text = itemArray[indexPath.row]
+           
+       return cell
+    }
+    
+}
+
